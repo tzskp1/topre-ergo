f0a2ef1db2ab85dce99f93bf7fbcd6cee92b5b32
Kazunari Tanaka <tzskp1@gmail.com>
2020-04-18 12:21:04 +0900

    improve

--- topplate.py
+++ topplate.py
@@ -12,8 +12,16 @@ def parse(p):
         _, x, y = p
         return linear_extrude(1.4)(translate ([x, -y, 0.0])(square(14, True)))
 
+def parse_hole(p):
+    _, x, y = p
+    return linear_extrude(1.4)(translate ([x, -y, 0.0])(circle(1.1)))
+
 f = load(open("./right.kicad_pcb"))
 y = list(filter(lambda x: x[0] == Symbol("module") if isinstance(x, list) else False, f))
 z = list(filter(lambda x: x[1] == Symbol("base:ToprePad"), y))
 sws = list(map(lambda y: list(filter(lambda x: x[0] == Symbol("at") if isinstance(x, list) else False, y))[0], z))
-scad_render_to_file(union()(*list(map(parse, sws))), "holes.scad")
+z = list(filter(lambda x: x[1] == Symbol("MountingHole:MountingHole_2.2mm_M2"), y))
+holes = list(map(lambda y: list(filter(lambda x: x[0] == Symbol("at") if isinstance(x, list) else False, y))[0], z))
+
+u = union()(*(list(map(parse, sws)) + list(map(parse_hole, holes))))
+scad_render_to_file(u, "holes.scad")
