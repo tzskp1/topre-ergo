bf22e4c7c43bdb314dac098ff9d06c23ed7bf820
Kazunari Tanaka <tzskp1@gmail.com>
2020-05-21 18:37:02 +0900

    fix holes

--- topplate.py
+++ topplate.py
@@ -4,7 +4,7 @@ from sexpdata import load, Symbol
 from solid import *
 from solid.utils import *
 
-hole = polygon([[-7, 6.3], [-6.3, 7], [6.3, 7], [7, 6.3], [7, -6.3], [6.3, -7], [-6.3, -7], [-7, -6.3]])
+hole = polygon([[-7.25, 6.3], [-6.3, 7.25], [6.3, 7.25], [7.25, 6.3], [7.25, -6.3], [6.3, -7.25], [-6.3, -7.25], [-7.25, -6.3]])
 
 def parse(p):
     try:
@@ -16,7 +16,7 @@ def parse(p):
 
 def parse_hole(p):
     _, x, y = p
-    return (translate ([x, -y, 0.0])(circle(1.1)))
+    return (translate ([x, -y, 0.0])(circle(1.1, segments = 30)))
 
 f = load(open("./right.kicad_pcb"))
 y = list(filter(lambda x: x[0] == Symbol("module") if isinstance(x, list) else False, f))
